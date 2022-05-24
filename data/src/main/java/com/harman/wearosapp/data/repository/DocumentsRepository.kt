20f966750062bcc5500295de3efaa22b8ff2f330
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-05-24 16:42:41 +0300

    data export imp

--- data/src/main/java/com/harman/wearosapp/data/repository/DocumentsRepository.kt
+++ data/src/main/java/com/harman/wearosapp/data/repository/DocumentsRepository.kt
@@ -1,6 +1,7 @@
 package com.harman.wearosapp.data.repository
 
 import android.content.Context
+import android.os.Environment
 import com.harman.data.R
 import com.harman.wearosapp.domain.model.HRRecordModel
 import com.harman.wearosapp.domain.repository.ExportState
@@ -10,13 +11,15 @@ import java.io.File
 import java.io.FileWriter
 import java.io.IOException
 
+
 class DocumentsRepository(private val context: Context): IDocumentsRepository {
 
     override fun saveHRValuesToCsv(hrRecords: List<HRRecordModel>): ExportState {
         if (hrRecords.isEmpty()) return ExportState.Failure(context.getString(R.string.no_value_msg))
 
+        val root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         return try {
-            val file = File(context.filesDir, "HrRecord-${hrRecords.last().timestamp}")
+            val file = File(root, "HrRecord-${hrRecords.last().timestamp}.csv")
             val fw = FileWriter(file.absoluteFile)
             val bw = BufferedWriter(fw)
             hrRecords.forEach {
